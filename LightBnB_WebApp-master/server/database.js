998803b3357722bb5a9e7d0725cd4670460c7a3c
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-26 20:34:02 +0000

    add SQL query to search function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -94,11 +94,46 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = function(options, limit) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  ;`, [limit])
-  .then(res => (res.rows));
+  
+  const queryParams = [];
+  
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  
+  let addAND = false;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+    addAND = true;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
+    queryString += `${addAND? 'AND':'WHERE'} cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length}`;
+    addAND = true;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `${addAND? 'AND':'WHERE'} rating >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  
+  console.log(queryString, queryParams);
+      
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
 } 
 
 exports.getAllProperties = getAllProperties;
@@ -108,7 +143,7 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+  const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
